ee4a83010fc5fda69b3b6712a20858b03f40381f
blessed234640 <Arturazimov200577@gmail.com>
2025-11-07 14:31:00 +0500

    Initial commit: Django snake shop - Python Palace - Complete shop application with product catalog - Modern responsive design with snake theme - Category and product models with image support - Custom CSS with gradient design and animations - Ready for e-commerce functionality

---
+++ cart/cart.py
@@ -0,0 +1,65 @@
+from decimal import Decimal
+
+from django.conf import settings
+from shop.models import Product
+
+
+class Cart:
+    def __init__(self, request):  # ИНИЦИАЛИЗИРОВАТЬ КОРЗИНУ
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # СОХРАНИТЬ ПУСТУЮ КОРЗИНУ В СЕАНСЕ
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def add(
+        self, product, quantity=1, override_quantity=False
+    ):  # ДОБАВИТЬ ТОВАР В КОРЗИНУ ЛИБО ОБНОВИТЬ ЕГО КОЛИЧЕСТВО
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                "quantity": 0,
+                "price": str(product.price),
+            }
+        if override_quantity:
+            self.cart[product_id]["quantity"] = quantity
+        else:
+            self.cart[product_id]["quantity"] += quantity
+        self.save()
+
+    def save(self):  # пометитиь сеанс как "измененный", чтобы обеспечить его созранение
+        self.session.modified = True
+
+    def remove(self, product):  # удалить товар из корзины
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(
+        self,
+    ):  # прокрутитьь товарные позиции корзины в цикле и получить товары из бдшки
+        product_ids = (
+            self.cart.keys()
+        )  # получить обьекты product И добавить их в корзину
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]["product"] = product
+            for item in cart.values():
+                item["price"] = Decimal(item["price"])
+                item["total_price"] = item["price"] * item["quantity"]
+                yield item
+
+    def __len__(self):  # подсчитать все товарные позиции в корзине
+        return sum(item["quantity"] for item in self.cart.values())
+
+    def get_total_price(self):
+        return sum(
+            Decimal(item["price"]) * item["quantity"] for item in self.cart.values()
+        )
+
+    def clear(self):  # удалить корзину из сеанса
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
