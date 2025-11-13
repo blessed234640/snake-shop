24da6aab749bf2c069a46430bc64e4c2f5c24948
blessed234640 <Arturazimov200577@gmail.com>
2025-11-14 03:34:57 +0500

    Update cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 
+from coupons.models import Coupon
 from django.conf import settings
 from shop.models import Product
 
@@ -12,6 +13,8 @@ class Cart:
             # СОХРАНИТЬ ПУСТУЮ КОРЗИНУ В СЕАНСЕ
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # сохраняйте текущий примененный купон
+        self.coupon_id = self.session.get("coupon_id")
 
     def add(
         self, product, quantity=1, override_quantity=False
@@ -65,3 +68,20 @@ class Cart:
     def clear(self):  # удалить корзину из сеанса
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
