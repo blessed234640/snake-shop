8a79918fe95e0d112f5f2e0f37912dc9a67a9190
blessed234640 <Arturazimov200577@gmail.com>
2025-11-23 18:11:07 +0500

    an internalization system was created, a 3-language support system was added, English, Russian, and Spanish were changed, templates, the initial screen, the shopping cart, and much more were changed

--- myshop/settings.py
+++ myshop/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from decouple import config
+from django.utils.translation import gettext_lazy as _
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,11 +23,13 @@ INSTALLED_APPS = [
     "payment.apps.PaymentConfig",
     "shop.apps.ShopConfig",
     "coupons.apps.CouponsConfig",
+    'rosetta',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -47,6 +50,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
                 "cart.context_processors.cart",
             ],
         },
@@ -76,12 +80,15 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
-LANGUAGE_CODE = "en-us"
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
+LANGUAGE_CODE = "ru"
 
 LANGUAGES = [
-    ("en", "English"),
-    ("ru", "Russian"),
+    ("en", _("English")),
+    ("ru", _("Russian")),
+    ("es", _("Spanish")),
 ]
 
 TIME_ZONE = "UTC"
