a09d2d70367dc1b2438232ee2e40179c82d02bb4
blessed234640 <Arturazimov200577@gmail.com>
2025-11-24 02:03:59 +0500

    delete webhook from urls payment and update another languages

--- payment/urls.py
+++ payment/urls.py
@@ -6,8 +6,7 @@ from . import views, webhooks
 app_name = "payment"
 
 urlpatterns = [
-    path(_('process/'), views.payment_process, name="process"),
-    path(_('completed/'), views.payment_completed, name="completed"),
-    path(_('canceled/'), views.payment_canceled, name="canceled"),
-    path("webhook/", webhooks.stripe_webhook, name="stripe-webhook"),
+    path(_("process/"), views.payment_process, name="process"),
+    path(_("completed/"), views.payment_completed, name="completed"),
+    path(_("canceled/"), views.payment_canceled, name="canceled"),
 ]
